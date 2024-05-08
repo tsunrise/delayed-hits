09c1f2e1c92003c1faf55e3e1f45d89f90875414
Tom <me@tomshen.io>
2024-05-07 23:30:06 -0700

    start working on pcap loader

--- proj-experiments/src/data.rs
+++ proj-experiments/src/data.rs
@@ -7,7 +7,9 @@ pub fn load_example_events(path: &str) -> RequestEvents<u32> {
 }
 
 #[allow(dead_code)]
-pub fn load_network_traces(path: &str) -> RequestEvents<(u32, u16)> {
+/// Load network traces from a pcap file.
+/// Each object ID represents a flow, and the tuple represents (src, dst, port).
+pub fn load_network_traces(path: &str) -> RequestEvents<(u32, u32, u16)> {
     let file = std::fs::File::open(path).unwrap();
     let reader = std::io::BufReader::new(file);
     bincode::deserialize_from(reader).unwrap()
