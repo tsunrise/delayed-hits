e17f6fce352edd6562f3912d2b20124d52676865
Tom <me@tomshen.io>
2024-05-20 16:08:59 -0700

    compute maximum active objects

---
+++ proj-cache-sim/src/heuristics.rs
@@ -0,0 +1,59 @@
+//! Utilities for calculating the optimal cache size for a given workload.
+
+use ahash::{AHashMap, AHashSet};
+use proj_models::RequestEvent;
+
+use crate::cache::ObjectId;
+
+/// Get the maximum number of overlapping live ranges of objects in the workload at time. A live range is
+/// an interval from the first access to the last access of an object.
+pub fn maximum_active_objects<T: ObjectId>(events: &[RequestEvent<T>]) -> usize {
+    let mut last_access = AHashMap::new();
+    for event in events {
+        last_access.insert(event.key.clone(), event.timestamp);
+    }
+    let mut active_objects = AHashSet::new();
+    let mut max_active_objects = 0;
+    for event in events {
+        active_objects.insert(event.key.clone());
+        if let Some(last_access_time) = last_access.get(&event.key) {
+            if event.timestamp == *last_access_time {
+                active_objects.remove(&event.key);
+                last_access.remove(&event.key);
+            }
+        }
+        max_active_objects = max_active_objects.max(active_objects.len());
+    }
+    max_active_objects
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_maximum_active_objects() {
+        let events = [
+            (1, 1),
+            (2, 2),
+            (3, 3),
+            (1, 4),
+            (2, 5),
+            (3, 6),
+            (1, 7),
+            (2, 8),
+            (3, 9),
+            (4, 10),
+            (5, 11),
+            (4, 12),
+        ]
+        .iter()
+        .map(|(key, timestamp)| RequestEvent {
+            key: *key,
+            timestamp: *timestamp,
+        })
+        .collect::<Vec<_>>();
+
+        assert_eq!(maximum_active_objects(&events), 3);
+    }
+}
--- proj-cache-sim/src/lib.rs
+++ proj-cache-sim/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod cache;
+pub mod heuristics;
 pub mod macros;
 pub mod simulator;
 pub mod types;
