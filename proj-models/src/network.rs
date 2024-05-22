c1ce933eecfa44c4b573c280485fefa3b71010c1
Tom <me@tomshen.io>
2024-05-22 16:52:28 -0700

    use only ips for flows

--- proj-models/src/network.rs
+++ proj-models/src/network.rs
@@ -1,5 +1,6 @@
 use std::net::Ipv4Addr;
 
+use derivative::Derivative;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Eq, PartialEq, Hash, Debug, Clone, Copy, Serialize, Deserialize)]
@@ -8,11 +9,18 @@ pub enum Protocol {
     UDP,
 }
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, Serialize, Deserialize)]
+#[derive(Derivative, Debug, Clone, Copy, Serialize, Deserialize)]
+#[derivative(Hash, Eq, PartialEq)]
 pub struct Flow {
     pub src_ip: Ipv4Addr,
     pub dst_ip: Ipv4Addr,
+    #[derivative(Hash = "ignore")]
+    #[derivative(PartialEq = "ignore")]
     pub src_port: u16,
+    #[derivative(Hash = "ignore")]
+    #[derivative(PartialEq = "ignore")]
     pub dst_port: u16,
+    #[derivative(Hash = "ignore")]
+    #[derivative(PartialEq = "ignore")]
     pub protocol: Protocol,
 }
