5695f7d72ef65a114a6cd4d25b89b3bfb7969bda
Tom <me@tomshen.io>
2024-06-04 23:21:23 -0700

    fix download script

--- results/tokyo_wheat17_0529/download.sh
+++ results/tokyo_wheat17_0529/download.sh
@@ -1,4 +1,4 @@
-wget https://r2.tomshen.io/class_host/cs244/tokyo_wheat17_0529.tar.gz
+curl https://r2.tomshen.io/proj_host/cs244/tokyo_wheat17_0529.tar.gz -o tokyo_wheat17_0529.tar.gz
 sha1sum -c *.sha1
 tar -xvf tokyo_wheat17_0529.tar.gz
 rm tokyo_wheat17_0529.tar.gz
